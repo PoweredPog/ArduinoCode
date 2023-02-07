eb00df6353ac2bbc679df4e7bb0ae0f2dc6a7aab
PoweredPog <117055027+PoweredPog@users.noreply.github.com>
2023-02-07 12:35:23 -0800

    Update and rename C++ Calc.cpp to main.cpp
    added clear function, minor bug fixes

--- main.cpp
+++ main.cpp
@@ -12,15 +12,18 @@ string string_precision(double x, int y){
     stream<<fixed<<setprecision(y)<<x;
     return stream.str();
 }
+void clear() {
+    // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
+    cout << "\x1B[2J\x1B[H";
+}
 int main(){
-    double pvTot,dayRange,priceKwh; 
-    string acc;
-    cout << "Enter account name: \n";
-    cin >> acc;
     power_acc alpha;
+    cout << "Enter account name: \n";
+    cin >> alpha.acc_name;
     cout << "Please enter Total Energy in Watts, the amount of days, and the price per Kwh: \n";
     cin >> alpha.watt_total;
     cin >> alpha.daysTotal;
     cin >> alpha.price;
-    cout << alpha.to_string();
+    clear();
+    cout <<alpha.to_string();
 }
